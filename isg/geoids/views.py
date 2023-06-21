5a45604b8cf54160a40506e2aeb8b5271fa1d984
JFToro192 <juanfertoro192@gmail.com>
2023-06-21 21:56:59 +0200

    inital template geoids

--- isg/geoids/views.py
+++ isg/geoids/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template("geoids/index.html")
+    context = {
+        'sample_text': 'ISG WebGIS'
+    }
+    return HttpResponse(template.render(context, request))
